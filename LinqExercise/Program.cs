9a4c17ec550e16bdb77133dc72b7af60d264b027
calebtripp <calebtripp@outlook.com>
2022-09-30 12:04:14 -0400

    2 Tasks Complete

--- LinqExercise/Program.cs
+++ LinqExercise/Program.cs
@@ -1,50 +1,43 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace LinqExercise
 {
     class Program
-    {
-        //Static array of integers
+    {      
         private static int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-
         static void Main(string[] args)
-        {
-            /*
-             * 
-             * Complete every task using Method OR Query syntax. 
-             * You may find that Method syntax is easier to use since it is most like C#
-             * Every one of these can be completed using Linq and then printing with a foreach loop.
-             * Push to your github when completed!
-             * 
-             */
+        {// work below
+         // numbers.Sum().ToString => Console.WriteLine(numbers.Sum);
 
-            //TODO: Print the Sum of numbers
+            numbers.Sum();
+            Console.WriteLine(numbers.Sum());
+            //numbers.ForEach(numbers => Console.WriteLine(numbers));  is foreach not usable for this first one? dont see a need for it.
+                                
+           // numbers.Average();
+            Console.WriteLine(numbers.Average());
 
-            //TODO: Print the Average of numbers
+            //numbers.OrderBy(x => x);
+            //numbers.OrderByDescending(x => x);
 
-            //TODO: Order numbers in ascending order and print to the console
 
-            //TODO: Order numbers in decsending order adn print to the console
+            //reference
+       //vGList.OrderBy(vGList => vGList.Length)
+       //.ToList()
+       //.ForEach(vGList => Console.WriteLine(vGList));
 
-            //TODO: Print to the console only the numbers greater than 6
 
-            //TODO: Order numbers in any order (acsending or desc) but only print 4 of them **foreach loop only!**
 
-            //TODO: Change the value at index 4 to your age, then print the numbers in decsending order
+
+
+
 
             // List of employees ****Do not remove this****
             var employees = CreateEmployees();
 
-            //TODO: Print all the employees' FullName properties to the console only if their FirstName starts with a C OR an S and order this in acesnding order by FirstName.
-
-            //TODO: Print all the employees' FullName and Age who are over the age 26 to the console and order this by Age first and then by FirstName in the same result.
-
-            //TODO: Print the Sum and then the Average of the employees' YearsOfExperience if their YOE is less than or equal to 10 AND Age is greater than 35
-
-            //TODO: Add an employee to the end of the list without using employees.Add()
-
+          
 
             Console.WriteLine();
 
